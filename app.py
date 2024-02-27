c2069443ac884266c83538326289885791c07337
LuisMCota <luis.f.m.c@outlook.com>
2024-02-26 19:50:37 -0600

    search

--- app.py
+++ app.py
@@ -8,13 +8,29 @@ GOOGLE_PLACES_API_KEY = 'AIzaSyAj0VJtl8C6oDTyJnIOqGjppUfTg0VeAO8'
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+        address = request.form['address']
         query = request.form['query']
-        results = search_places(query)
-        return render_template('results.html', results=results)
+        location = get_location_from_address(address)
+        if location:
+            results = search_places(query, location)
+            return render_template('results.html', results=results)
+        else:
+            error = "No se pudo encontrar la ubicación. Intenta con otra dirección."
+            return render_template('home.html', error=error)
     return render_template('home.html')
 
-def search_places(query):
-    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={GOOGLE_PLACES_API_KEY}"
+def get_location_from_address(address):
+    geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={GOOGLE_PLACES_API_KEY}"
+    response = requests.get(geocode_url)
+    data = response.json()
+    if data['status'] == 'OK':
+        location = data['results'][0]['geometry']['location']
+        return f"{location['lat']},{location['lng']}"
+    else:
+        return None
+
+def search_places(query, location, radius=500):
+    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&location={location}&radius={radius}&key={GOOGLE_PLACES_API_KEY}"
     response = requests.get(url)
     data = response.json()
     return data['results']
